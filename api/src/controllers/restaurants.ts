5315c428133bec4230dc7777ba78b90e0e4c5c4a
radheshyam <radheshyamjio52@gmail.com>
2024-06-29 20:17:34 +0530

    complete api for update and get restaurent

--- api/src/controllers/restaurants.ts
+++ api/src/controllers/restaurants.ts
@@ -5,6 +5,13 @@ import ErrorHandler from "../utils/ErrorHandler"
 import mongoose from "mongoose"
 
 
+async function uploadImage(image: Express.Multer.File) {
+    const base64Image = Buffer.from(image.buffer).toString('base64')
+    const dataURI = "data:" + image.mimetype + ";base64," + base64Image
+    const uploadRes = await cloudinary.v2.uploader.upload(dataURI, { timeout: 6000000 });
+    return uploadRes.url
+}
+
 export async function createRestaurants(req: Request, res: Response, next: NextFunction) {
     try {
         const userId = req.body.userId
@@ -14,23 +21,78 @@ export async function createRestaurants(req: Request, res: Response, next: NextF
             return next(ErrorHandler(409, "Already have an restaurant"))
         }
 
-        const image = req.file as Express.Multer.File
-        const base64Image = Buffer.from(image.buffer).toString('base64')
-        const dataURI = "data:" + image.mimetype + ";base64," + base64Image
-        const uploadRes = await cloudinary.v2.uploader.upload(dataURI,{timeout:6000000});
-        const imageUrl = uploadRes.url
+        const imageUrl = await uploadImage(req.file as Express.Multer.File)
 
         const newRestaurent = new Restaurant(req.body)
-        console.log(req.body)
         newRestaurent.imageUrl = imageUrl
         newRestaurent.user = new mongoose.Types.ObjectId(userId)
-
         await newRestaurent.save()
 
         res.status(200).json({ restaurent: newRestaurent, message: "Resaurant Added Successfully" })
-    } catch (error:any) {
+    } catch (error: any) {
         next(error)
         console.log(error)
     }
 
+}
+export async function getRestaurant(req: Request, res: Response, next: NextFunction) {
+    const user = req.params.userId?.toString()
+    const id = req.userId
+console.log(user,id)
+    if (id !== user) {
+        return next(ErrorHandler(401, "Unauthorised"))
+    }
+
+    try {
+        const restaurant = await Restaurant.findOne({user})
+        console.log(restaurant,"hi")
+        if (!restaurant) {
+            return next(ErrorHandler(400, "You have no restaurent"))
+        }
+        res.status(200).json(restaurant)
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export async function updateRestaurant(req: Request, res: Response, next: NextFunction) {
+    const userId = req.params.userId
+    const id = req.userId
+    const {restaurantName,city,country,deliveryPrice,estimatedDeliveryTime,cuisines,menuItems } = req.body
+    console.log(menuItems)
+    if (id !== userId) {
+        return next(ErrorHandler(401, "Unauthorised"))
+    }
+
+    try {
+        const restaurant = await Restaurant.findOne({ user: userId })
+        if (!restaurant) {
+            return next(ErrorHandler(400, "You have no restaurent"))
+        }
+
+        let imageUrl
+
+        if (req.file) {
+            imageUrl = await uploadImage(req.file as Express.Multer.File)
+        }
+
+        await Restaurant.findOneAndUpdate({
+            user: userId,
+            $set: {
+                ...(restaurantName && {restaurantName}),
+                ...(city && {city}),
+                ...(country && {country}),
+                ...(deliveryPrice && {deliveryPrice}),
+                ...(estimatedDeliveryTime && {estimatedDeliveryTime}),
+                ...(cuisines && {cuisines}),
+                ...(menuItems && {menuItems}),
+                ...(imageUrl && { imageUrl })
+            }
+        })
+        const updateRestaurant = await Restaurant.findOne({user:userId})
+        res.status(200).json({restaurant:updateRestaurant,message:"Restaurant updated"})
+    } catch (error) {
+        next(error)
+    }
 }
--- api/src/routes/restaurants.ts
+++ api/src/routes/restaurants.ts
@@ -1,6 +1,6 @@
 import express from "express";
 // import VerifyToken from "../utils/VerifyToken";
-import { createRestaurants } from "../controllers/restaurants";
+import { createRestaurants, getRestaurant, updateRestaurant } from "../controllers/restaurants";
 import multer from "multer"
 import VerifyToken from "../utils/VerifyToken";
 const router = express.Router()
@@ -12,9 +12,10 @@ const upload = multer({
     limits:{
         fileSize:5 * 1024 * 1024 //5mb
     }
-
 })
 router.post('/create',upload.single('imageFile'),VerifyToken,createRestaurants)
+router.put('/update/:userId',upload.single('imageFile'),VerifyToken,updateRestaurant)
+router.get('/get/:userId',VerifyToken,getRestaurant)
 export default router
 
 
