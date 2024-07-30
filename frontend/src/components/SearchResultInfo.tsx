fa6833afd980fb2ae9201c8a13f87f11fb28ee13
radheshyam <radheshyamjio52@gmail.com>
2024-07-30 20:30:27 +0530

    added search cards

---
+++ frontend/src/components/SearchResultInfo.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+
+
+type Props = {
+  total:number,
+  city:string
+}
+
+const SearchResultInfo = ({total,city}:Props) => {
+  return (
+    <div className="flex text-xl font-bold flex-col gap-3 justify-between lg:items-center lg:flex-row">
+      <span>
+        {total} Restaurants Found in {city}
+        <Link  to={'/'} className=" ml-1 text-blue-500 text-sm underline cursor-pointer font-semibold">change location</Link>
+      </span>
+
+      <span>dropdown here</span>
+    </div>
+  )
+}
+
+export default SearchResultInfo
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,20 +1,89 @@
-import { useAppDispatch} from "@/redux/hooks/hooks"
+import SearchResultCards from "@/components/SearchResultCards"
+import SearchResultInfo from "@/components/SearchResultInfo"
+import { restaurentType } from "@/configs/schema"
+import { useAppDispatch, useAppSelector } from "@/redux/hooks/hooks"
 import { hideHero } from "@/redux/slices/userSlice"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { toast } from "sonner"
+
+
+type resultType = {
+    data: [restaurentType]
+    pagination: {
+        total: number,
+        page: number,
+        pages: number
+    }
+}
 
 const SearchPage = () => {
 
     const dispatch = useAppDispatch()
-
+    const { baseUrl } = useAppSelector(state => state.User)
+    const [results, setResults] = useState<resultType>()
     useEffect(() => {
         dispatch(hideHero())
     }, [])
 
-    const searchQuery = useParams().searchQuery
+    const city = useParams().searchQuery
+
+
+    async function searchRestaurants(searchTerm: string) {
+
+        try {
+            const res = await fetch(`${baseUrl}/api/search/${searchTerm}`, {
+                method: "GET",
+                credentials: "include"
+
+            })
+
+            const data = await res.json()
+
+
+            if (res.ok) {
+                setResults(data)
+                console.log(results)
+            }
+
+        } catch (error: any) {
+            toast.error(error.message)
+            throw new Error(error)
+        }
+
+    }
+
+    useEffect(() => {
+        if (city) {
+            searchRestaurants(city as string)
+        }
+    }, [])
+
+
+    if (!results?.data || !city) {
+        return <span>No result Found</span>
+    }
+
     return (
-        <div>
-            <p>{searchQuery}</p>
+        <div className=" grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+
+
+            <div id="cuisines_list">
+                insert cusines here
+            </div>
+
+            <div id="main_content" className=" flex flex-col gap-5">
+                <SearchResultInfo total={results.pagination.total} city={city} />
+                {
+                    results.data.map((restaurant,index) => {
+                        return (
+
+                            <SearchResultCards key={index} restaurant={restaurant}/>
+    
+)
+                    })
+                }
+            </div>
         </div>
     )
 }
